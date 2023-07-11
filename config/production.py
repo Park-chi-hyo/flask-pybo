99d7f779ece8cac12b0fefbdf0729c8ad7bdaf1d
pch <mightmars@hanmail.net>
2023-07-11 10:32:50 +0900

    config 변경

--- config/production.py
+++ config/production.py
@@ -12,7 +12,7 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     db=os.getenv('DB_NAME'))
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
+SECRET_KEY = b'c\r\x11\xb0RQ\x06\x05y\xda\xf6.\x81"a\xb0'
 
 dictConfig({
     'version': 1,
